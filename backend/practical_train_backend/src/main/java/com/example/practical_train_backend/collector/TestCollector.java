73c381a7e1a3234583e74f46e164ade6fe388c2e
kjhjk566 <74780868+kjhjk566@users.noreply.github.com>
2024-08-27 15:21:15 +0800

    create spring boot

---
+++ backend/practical_train_backend/src/main/java/com/example/practical_train_backend/collector/TestCollector.java
@@ -0,0 +1,17 @@
+package com.example.practical_train_backend.collector;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/user")
+public class TestCollector {
+
+    @GetMapping("/test")
+    public String hello() {
+
+        return"hello";
+    }
+}
---
+++ backend/practical_train_backend/src/test/java/com/example/practical_train_backend/PracticalTrainBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practical_train_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalTrainBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
