9eeacc9dfd53b42aa9ee6bd802ed3b6bd7552687
陈子昂 <1812432358@qq.com>
2024-08-28 21:40:59 +0800

    Add:添加后端注册代码. Closes #10.

--- backend/practical_train_backend/src/main/java/com/example/practical_train_backend/dao/UserMapper.java
+++ backend/practical_train_backend/src/main/java/com/example/practical_train_backend/dao/UserMapper.java
@@ -3,8 +3,7 @@ package com.example.practical_train_backend.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.practical_train_backend.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -13,4 +12,13 @@ public interface UserMapper extends BaseMapper<User> {
     @Select("SELECT * FROM user")
     List<User> getAll();
 
+    // 根据用户名查找用户
+    @Select("SELECT * FROM user WHERE username = #{username}")
+    User findByUsername(@Param("username") String username);
+
+    // 插入新用户
+    @Insert("INSERT INTO user (username, password,email,department) VALUES (#{username}, #{password}, #{email}, #{department})")
+    @Options(useGeneratedKeys = true, keyProperty = "id") // 如果你的数据库主键是自增的，自动生成ID
+    int insertUser(User user);
 }
+
