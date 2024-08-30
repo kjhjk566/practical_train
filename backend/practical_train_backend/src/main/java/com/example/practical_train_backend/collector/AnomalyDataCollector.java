9507a4c0e49f3708ee5afe49121f6485353d56f9
kjhjk566 <74780868+kjhjk566@users.noreply.github.com>
2024-08-30 23:52:33 +0800

    Add: 添加数据库结果

--- backend/practical_train_backend/src/main/java/com/example/practical_train_backend/collector/AnomalyDataCollector.java
+++ backend/practical_train_backend/src/main/java/com/example/practical_train_backend/collector/AnomalyDataCollector.java
@@ -3,6 +3,7 @@ package com.example.practical_train_backend.collector;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
+import com.example.practical_train_backend.dao.ExperimentMapper;
 import com.example.practical_train_backend.entity.AnomalyInfo;
 import com.example.practical_train_backend.entity.Experiment;
 import com.example.practical_train_backend.entity.MTS;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 
 
 import com.alibaba.fastjson.JSONObject;
+
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -21,6 +24,9 @@ import java.util.List;
 public class AnomalyDataCollector {
     @Autowired
     private AnomalyDataService anomalyDataService;
+
+    @Autowired
+    private ExperimentMapper experimentMapper;
 
     @GetMapping("/get_all")
     public ResponseVO<List<Experiment>> getFile(){
@@ -42,15 +48,20 @@ public class AnomalyDataCollector {
     }
 
     @PostMapping("/inject")
-    public ResponseVO<MTS> inject(@RequestBody AnomalyInfo anomalyInfo){
-        JsonUtil jsonUtil = new JsonUtil();
-        String myPath = "templateData/normal_1.json";
-        String path = JsonUtil.class.getClassLoader().getResource(myPath).getPath();
-        String s = jsonUtil.readJsonFile(path);
+    public ResponseVO<double [] []> inject(@RequestBody AnomalyInfo anomalyInfo){
+//        JsonUtil jsonUtil = new JsonUtil();
+//        String myPath = "templateData/normal_1.json";
+//        String path = JsonUtil.class.getClassLoader().getResource(myPath).getPath();
+        if (! experimentMapper.ifExists(anomalyInfo.getUsername(),anomalyInfo.getSourceName())){
+            return ResponseVO.error("not get data");
+        }
+        Experiment e = experimentMapper.getByUsernameAndExperimentName(anomalyInfo.getUsername(),anomalyInfo.getSourceName());
+
+        String s = e.getTimeSeriesData();
         JSONObject jobj = JSON.parseObject(s);
-        JSONObject experiment = jobj.getJSONObject("normal_1");
+        JSONObject experimentJson = jobj.getJSONObject(anomalyInfo.getSourceName());
 //        JSONObject data = experiment.getJSONObject("data");
-        JSONArray jsonArray = experiment.getJSONArray("data");
+        JSONArray jsonArray = experimentJson.getJSONArray("data");
         // 遍历JSON数组，提取二维数组中的数据
         double[][] doubleArray = new double[jsonArray.size()][];
 
@@ -99,8 +110,21 @@ public class AnomalyDataCollector {
                 anomalyDataService.collectiveSeasonalOutliers(mts, index, anomalyInfo.getSeasonalRatio(), anomalyInfo.getSeasonalFactor(), anomalyInfo.getSeasonalRadius());
             }
         }
+        experimentJson.put("data",Arrays.toString(mts.getData()));
+        jobj.put(anomalyInfo.getLabName(),experimentJson);
+        jobj.remove(anomalyInfo.getSourceName());
+        Experiment experiment = new Experiment();
+        experiment.setUsername(anomalyInfo.getUsername());
+        experiment.setExperimentName(anomalyInfo.getLabName());
+        experiment.setSmoothness((Double) experimentJson.get("smoothness"));
+        experiment.setPeriodicity((Double)experimentJson.get("periodicity"));
+        experiment.setCorrelation((Double)experimentJson.get("correlation"));
+        experiment.setMetricNum((Integer) experimentJson.get("metricNum"));
+        experiment.setTimeSeriesData(jobj.toString());
+        experimentMapper.insertExperiment(experiment);
 
-        return ResponseVO.success(mts);
+
+        return ResponseVO.success(Arrays.toString(mts.getData()));
     }
 
 }
